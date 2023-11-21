3b5c40137cae8df9094c1347cc3184e0b3f7c1e3
HongboDoll <101861734+HongboDoll@users.noreply.github.com>
2023-11-21 01:05:52 +0100

    Update gatk.sh

--- Read mapping and variant calling/gatk.sh
+++ Read mapping and variant calling/gatk.sh
@@ -9,7 +9,7 @@ ls mapping/*bam | awk -F '/' '{print $NF}' | awk -F '.' '{print $1}' | while rea
 do
     echo """#!/bin/bash
     cd /media/bulk_01/users/li322/work/02_137_atlas_potato_reseq
-    gatk --java-options \"-Xmx10G -XX:ParallelGCThreads=1\" HaplotypeCaller -R $ref --sample-ploidy 4 --emit-ref-confidence GVCF -I mapping/${i}.sort.markdup.bam -O gvcf/${i}.sample.g.vcf.gz --native-pair-hmm-threads 1 # --sample-ploidy assumes samples are polyploidy
+    gatk --java-options \"-Xmx10G -XX:ParallelGCThreads=$threads\" HaplotypeCaller -R $ref --sample-ploidy 4 --emit-ref-confidence GVCF -I mapping/${i}.sort.markdup.bam -O gvcf/${i}.sample.g.vcf.gz --native-pair-hmm-threads $threads # --sample-ploidy assumes samples are polyploidy
     """ > ${i}_gatk.sh
 done
 
@@ -24,9 +24,9 @@ rm vcf.list
 grep '>' $ref | grep chr | sed 's/>//g' | while read i
 do
     echo """#!/bin/bash
-    gatk --java-options \"-Xmx50G -XX:ParallelGCThreads=7\" GenomicsDBImport -R $ref -L $i --sample-name-map sample.gvcf.xls --genomicsdb-workspace-path ${i}.gatk.db
+    gatk --java-options \"-Xmx10G -XX:ParallelGCThreads=$threads\" GenomicsDBImport -R $ref -L $i --sample-name-map sample.gvcf.xls --genomicsdb-workspace-path ${i}.gatk.db
 
-    gatk --java-options \"-Xmx50G -XX:ParallelGCThreads=7\" GenotypeGVCFs -R $ref --allow-old-rms-mapping-quality-annotation-data --sample-ploidy 4 -V gendb://${i}.gatk.db -O ${i}.gatk.vcf # --sample-ploidy
+    gatk --java-options \"-Xmx10G -XX:ParallelGCThreads=$threads\" GenotypeGVCFs -R $ref --allow-old-rms-mapping-quality-annotation-data --sample-ploidy 2 -V gendb://${i}.gatk.db -O ${i}.gatk.vcf # --sample-ploidy
     echo -e \"$PWD/${i}.gatk.vcf\" >> vcf.list
     """ > ${i}_run.sh && chmod 755 ${i}_run.sh
 done
@@ -34,50 +34,47 @@ done
 
 for i in {01..12}
 do
+echo """#!/bin/bash
+	
 gatk SelectVariants -select-type SNP --restrict-alleles-to BIALLELIC -V chr${i}.gatk.vcf -O chr${i}.gatk.snp.vcf.gz
 
-gatk --java-options "-Xmx200G -XX:ParallelGCThreads=70" VariantFiltration -V chr${i}.gatk.snp.vcf.gz \
---filter-expression "QD < 2.0" --filter-name "LowQD" \
---filter-expression "MQ < 40.0" --filter-name "MQ40.0" \
---filter-expression "FS > 60.0" --filter-name "FS60.0" \
---filter-expression "SOR > 3.0" --filter-name "SOR3.0" \
---filter-expression "MQRankSum < -12.5" --filter-name "MQRankSum-12.5" \
---filter-expression "ReadPosRankSum < -8.0" --filter-name "ReadPosRankSum-8.0" \
--G-filter "GQ < 20.0" -G-filter-name "GQ20.0" \
--O chr${i}.gatk.snp.filter.vcf.gz
+gatk --java-options \"-Xmx100G -XX:ParallelGCThreads=13\" VariantFiltration -V ${i}.gatk.snp.vcf.gz \
+--filter-expression \"QD < 2.0\" --filter-name \"LowQD\" \
+--filter-expression \"MQ < 40.0\" --filter-name \"MQ40.0\" \
+--filter-expression \"FS > 60.0\" --filter-name \"FS60.0\" \
+--filter-expression \"SOR > 3.0\" --filter-name \"SOR3.0\" \
+--filter-expression \"MQRankSum < -12.5\" --filter-name \"MQRankSum-12.5\" \
+--filter-expression \"ReadPosRankSum < -8.0\" --filter-name \"ReadPosRankSum-8.0\" \
+-G-filter \"GQ < 20.0\" -G-filter-name \"GQ20.0\" \
+-O ${i}.gatk.snp.filter.vcf.gz
 
-gatk SelectVariants --max-indel-size 30 --restrict-alleles-to BIALLELIC -select-type INDEL -V chr${i}.gatk.vcf -O chr${i}.gatk.indel.vcf.gz
+gatk SelectVariants --max-indel-size 30 --restrict-alleles-to BIALLELIC -select-type INDEL -V ${i}.gatk.vcf -O ${i}.gatk.indel.vcf.gz
 
-gatk --java-options "-Xmx200G -XX:ParallelGCThreads=70" VariantFiltration -V chr${i}.gatk.indel.vcf.gz \
---filter-expression "QD < 2.0" --filter-name "LowQD" \
---filter-expression "FS > 200.0" --filter-name "FS200" \
---filter-expression "SOR > 10.0" --filter-name "SOR10" \
---filter-expression "MQRankSum < -12.5" --filter-name "MQRankSum-12.5" \
---filter-expression "ReadPosRankSum < -8.0" --filter-name "ReadPosRankSum-8.0" \
--G-filter "GQ < 20.0" -G-filter-name "GQ20.0" \
--O chr${i}.gatk.indel.filter.vcf.gz
+gatk --java-options \"-Xmx100G -XX:ParallelGCThreads=13\" VariantFiltration -V ${i}.gatk.indel.vcf.gz \
+--filter-expression \"QD < 2.0\" --filter-name \"LowQD\" \
+--filter-expression \"MQ < 40.0\" --filter-name \"MQ40.0\" \
+--filter-expression \"FS > 200.0\" --filter-name \"FS200\" \
+--filter-expression \"SOR > 10.0\" --filter-name \"SOR10\" \
+--filter-expression \"MQRankSum < -12.5\" --filter-name \"MQRankSum-12.5\" \
+--filter-expression \"ReadPosRankSum < -8.0\" --filter-name \"ReadPosRankSum-8.0\" \
+-G-filter \"GQ < 20.0\" -G-filter-name \"GQ20.0\" \
+-O ${i}.gatk.indel.filter.vcf.gz
 
 gatk MergeVcfs \
--I chr${i}.gatk.snp.filter.vcf.gz \
--I chr${i}.gatk.indel.filter.vcf.gz \
--O chr${i}.gatk.filter.vcf
+-I ${i}.gatk.snp.filter.vcf.gz \
+-I ${i}.gatk.indel.filter.vcf.gz \
+-O ${i}.gatk.filter.vcf && rm ${i}.gatk.vcf ${i}.gatk.snp.filter.vcf.gz ${i}.gatk.indel.filter.vcf.gz
 
-rm chr${i}.gatk.vcf
-
-cat <(grep '#' chr${i}.gatk.filter.vcf | grep -v scaffold) <(grep -v '#' chr${i}.gatk.filter.vcf | grep -v scaffold | awk '$7=="PASS"') > t && mv t chr${i}.gatk.filter.vcf
+cat <(grep '#' ${i}.gatk.filter.vcf | grep -v scaffold) <(grep -v '#' ${i}.gatk.filter.vcf | grep -v scaffold | awk '\$7==\"PASS\"&&\$5!~/,/') > t && mv t ${i}.gatk.filter.vcf
 
 echo -e \"$PWD/${i}.gatk.filter.vcf\" >> vcf.filter.list
+""" > ${i}_run.sh && chmod 755 ${i}_run.sh
 
 done
 
-gatk MergeVcfs -I vcf.filter.list -O 166_potato_gatk.vcf.gz && rm chr*.gatk.filter.vcf chr*.gatk.vcf
-
-cat <(grep '#' chr${i}.gatk.filter.vcf) <(grep -v '#' chr${i}.gatk.filter.vcf | awk '$7=="PASS"&&$5!~/,/') | bgzip -f > chr${i}.gatk.filter.bi-allelic.vcf.gz
+gatk MergeVcfs -I vcf.filter.list -O 166_potato_Allchr.gatk.filter.vcf.gz && rm chr*.gatk.filter.vcf chr*.gatk.vcf
 
 tabix -f -p vcf chr${i}.gatk.filter.vcf.gz
-tabix -f -p vcf chr${i}.gatk.filter.bi-allelic.vcf.gz
-
-cat <(grep '#' chr01.gatk.filter.vcf) <(cat chr*.gatk.filter.vcf | grep -v '#') > 166_potato_Allchr.gatk.filter.vcf
 
 snp=`grep -v '#' 166_potato_Allchr.gatk.filter.vcf | awk 'length($4)==length($5)' | wc -l`
 indel=`grep -v '#' 166_potato_Allchr.gatk.filter.vcf | awk 'length($4)!=length($5)' | wc -l`
